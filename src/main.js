439880f8fc63103b6d074c8c066de96ff6a8da88
LHBUSA <sales@localhomebuyersusa.com>
2026-08-30 06:23:57 -0500

    Enable PropBetEdge article provenance layer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './styles/story-image-integrity.css';
 import { initSiteEnhancements } from './site-enhancements.js';
 import { initStoryMediaBackfill } from './media-backfill.js';
+import { initArticleTrustLayer } from './article-trust.js';
 import { initAnalytics } from './analytics.js';
 import { initRouter } from './router.js';
 
 initSiteEnhancements();
 initStoryMediaBackfill();
+initArticleTrustLayer();
 initAnalytics();
 initRouter();
