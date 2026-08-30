c7d1f73091c8e9918698b763a0f587793aa5f8ca
LHBUSA <sales@localhomebuyersusa.com>
2026-08-30 06:26:51 -0500

    Enable sport-aware article funnel

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './styles/story-image-integrity.css';
 import { initSiteEnhancements } from './site-enhancements.js';
 import { initStoryMediaBackfill } from './media-backfill.js';
 import { initArticleTrustLayer } from './article-trust.js';
+import { initArticleFunnel } from './article-funnel.js';
 import { initAnalytics } from './analytics.js';
 import { initRouter } from './router.js';
 
 initSiteEnhancements();
 initStoryMediaBackfill();
 initArticleTrustLayer();
+initArticleFunnel();
 initAnalytics();
 initRouter();
