4fb3d43bea5bcc00168a0ea1d17654a6a6b42d86
LHBUSA <sales@localhomebuyersusa.com>
2026-08-30 05:49:31 -0500

    Load story image integrity guard

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './styles/story-image-integrity.css';
 import { initSiteEnhancements } from './site-enhancements.js';
 import { initStoryMediaBackfill } from './media-backfill.js';
 import { initRouter } from './router.js';
